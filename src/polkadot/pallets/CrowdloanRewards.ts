3ac34b09e1de4e7f2b5253b6487012a070bc492b
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-01-29 22:42:42 +0500

    lets make storage work

--- src/polkadot/pallets/CrowdloanRewards.ts
+++ src/polkadot/pallets/CrowdloanRewards.ts
@@ -1,10 +1,9 @@
+import { addCall } from "@/store/extrinsics/slice";
 import { ApiPromise } from "@polkadot/api";
 import { Signer } from "@polkadot/api/types";
-import { stringToU8a } from "@polkadot/util";
 import { AnyAction, Dispatch } from "@reduxjs/toolkit";
+import { toHexString } from "../utils";
 import { PalletBase } from "./PalletBase";
-
-
 export class CrowdloanRewards extends PalletBase {
     constructor(api: ApiPromise, dispatch: Dispatch<AnyAction>) {
         super(api, dispatch);
@@ -15,12 +14,14 @@ export class CrowdloanRewards extends PalletBase {
      * @param rewardsAccount SS58 format string
      * @param contributorsAccount SS58 format string
      */
-    public async associate (proof: string, rewardAccount: string, contributorAccount: string | undefined = undefined) {
-        const rewardsAccountID = this.polkaApi.createType('AccountId32', rewardAccount);
+    public async associate(proof: string, rewardAccount: string, contributorAccount: string | undefined = undefined) {
+        const rewardsAccountID = this.polkaApi.createType("AccountId32", rewardAccount);
 
-        const association = !!contributorAccount ? {
-            RelayChain: [this.polkaApi.createType("AccountId32", contributorAccount), { Sr25519: proof }]
-        } : { Ethereum: proof }
+        const association = !!contributorAccount
+            ? {
+                  RelayChain: [this.polkaApi.createType("AccountId32", contributorAccount), { Sr25519: proof }],
+              }
+            : { Ethereum: proof };
 
         return this.polkaApi.tx.crowdloanRewards.associate(rewardsAccountID, association);
     }
@@ -28,10 +29,25 @@ export class CrowdloanRewards extends PalletBase {
      * Claim Rewards
      * @param rewardsAccount SS58 format string
      */
-     public async claim (rewardAccount: string, injectedSigner: Signer) {
-        const rewardAccountId = this.polkaApi.createType("AccountId32", rewardAccount);
-        return this.polkaApi.tx.crowdloanRewards.claim().signAndSend(rewardAccountId, {
-            signer: injectedSigner
-        })
+    public async claim(rewardAccount: string, injectedSigner: Signer) {
+        const rewardAccountId = this.polkaApi.createType("AccountId32", rewardAccount);    
+        const methodResult = await this.polkaApi.tx.crowdloanRewards.claim().signAndSend(rewardAccountId, {
+            signer: injectedSigner,
+        });
+        
+        this.dispatcher(
+            addCall({
+                call: {
+                    status: "PENDING",
+                    method: this.claim.name,
+                    pallet: "CrowdloanRewards",
+                    parameters: [rewardAccount],
+                    signer: rewardAccount,
+                    hash: toHexString(methodResult.hash),
+                },
+            })
+        );
+
+        return methodResult.toHuman();
     }
 }
