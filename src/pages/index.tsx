d0ef0a3392a104c26a172927c5cad85137d44d75
Saad Ahmed Siddiqui <48211799+saadjhk@users.noreply.github.com>
2022-03-16 13:59:34 +0500

    l

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,57 @@
-import CrowdloanRewardsUpdater from '@/polkadot/updaters/CrowdloanRewards';
-import { crowdLoanSignableMessage } from '@/polkadot/utils';
-import { stringToHex } from '@polkadot/util';
+import CrowdloanRewardsUpdater from '@/polkadot/updaters/CrowdloanRewards'
+import { crowdLoanSignableMessage } from '@/polkadot/utils'
+import { stringToHex } from '@polkadot/util'
 import type { NextPage } from 'next'
-import { useContext, useEffect, useState } from 'react';
-import { PicassoContext } from '@/polkadot/PicassoApiContext';
-import {useExtrinsics} from "substrate-react";
-import { APP_NAME } from '@/polkadot/constants';
+import { useContext, useEffect, useState } from 'react'
+import { PicassoContext } from '@/polkadot/PicassoApiContext'
+import { useExtrinsics } from 'substrate-react'
+import { APP_NAME } from '@/polkadot/constants'
 
 const Home: NextPage = () => {
-  const { api, accounts, crowdloanRewards } = useContext(PicassoContext);
-  const { extrinsics, txExecutor } = useExtrinsics();
-  const [signer, setSigner] = useState("");
-  const [reward, setReward] = useState("");
-  const [claim, setClaim] = useState("");
+  const { api, accounts, crowdloanRewards } = useContext(PicassoContext)
+  const { extrinsics, txExecutor } = useExtrinsics()
+  const [signer, setSigner] = useState('')
+  const [reward, setReward] = useState('')
+  const [claim, setClaim] = useState('')
 
   const onAssociate = async () => {
     if (api && crowdloanRewards) {
-      const { web3FromAddress, web3Enable } = require("@polkadot/extension-dapp");
-      await web3Enable(APP_NAME);
-      const injector = await web3FromAddress(signer);
-      const accId32 = api.createType("AccountId32", reward);
+      const {
+        web3FromAddress,
+        web3Enable,
+      } = require('@polkadot/extension-dapp')
+      await web3Enable(APP_NAME)
+      const injector = await web3FromAddress(signer)
+      const accId32 = api.createType('AccountId32', reward)
       if (injector.signer.signRaw) {
         const { signature } = await injector.signer.signRaw({
           address: signer,
           data: stringToHex(crowdLoanSignableMessage(accId32)),
-          type: 'bytes'
-        });
-        
-        const associate = await crowdloanRewards.associate(signature, reward, signer);
-        const response = await associate.send();
-        console.log(response.toHuman());
-      }
+          type: 'bytes',
+        })
 
+        const associate = await crowdloanRewards.associate(
+          signature,
+          reward,
+          signer,
+        )
+        const response = await associate.send()
+        console.log(response.toHuman())
+      }
     }
   }
 
   const onClaim = async () => {
     if (api && crowdloanRewards) {
-      const { web3FromAddress, web3Enable } = require("@polkadot/extension-dapp");
-      await web3Enable(APP_NAME);
-      const injector = await web3FromAddress(claim);
+      const {
+        web3FromAddress,
+        web3Enable,
+      } = require('@polkadot/extension-dapp')
+      await web3Enable(APP_NAME)
+      const injector = await web3FromAddress(claim)
 
       if (injector.signer && txExecutor) {
-        crowdloanRewards.claimExecute(claim, injector.signer, txExecutor);
+        crowdloanRewards.claimExecute(claim, injector.signer, txExecutor)
       }
     }
   }
@@ -54,33 +63,59 @@ const Home: NextPage = () => {
   return (
     <div>
       <label>Signer</label>
-      <select onChange={evt => {
-        setSigner(evt.target.value)
-      }}>
+      <select
+        onChange={(evt) => {
+          setSigner(evt.target.value)
+        }}
+      >
         {accounts.map((acc: any, index: number) => {
-          return (<option key={index} value={acc.address}>{acc.meta.name}</option>)
+          return (
+            <option key={index} value={acc.address}>
+              {acc.meta.name}
+            </option>
+          )
         })}
-      </select><br></br>
+      </select>
+      <br></br>
       <label>Reward</label>
-      <select onChange={evt => {
-        setReward(evt.target.value)
-      }}>
+      <select
+        onChange={(evt) => {
+          setReward(evt.target.value)
+        }}
+      >
         {accounts.map((acc: any, index: number) => {
-          return (<option key={index} value={acc.address}>{acc.meta.name}</option>)
+          return (
+            <option key={index} value={acc.address}>
+              {acc.meta.name}
+            </option>
+          )
         })}
-      </select><br></br>
-      <button onClick={evt => {
-        evt.preventDefault();
-        onAssociate();
-      }}>Associate</button><br></br>
+      </select>
+      <br></br>
+      <button
+        onClick={(evt) => {
+          evt.preventDefault()
+          onAssociate()
+        }}
+      >
+        Associate
+      </button>
+      <br></br>
       <label>Claim</label>
-      <select onChange={evt => {
-        setClaim(evt.target.value)
-      }}>
+      <select
+        onChange={(evt) => {
+          setClaim(evt.target.value)
+        }}
+      >
         {accounts.map((acc: any, index: number) => {
-          return (<option key={index} value={acc.address}>{acc.meta.name}</option>)
+          return (
+            <option key={index} value={acc.address}>
+              {acc.meta.name}
+            </option>
+          )
         })}
-      </select><br></br>
+      </select>
+      <br></br>
       <button onClick={onClaim}>Claim</button>
       <CrowdloanRewardsUpdater claimerAccount={signer} />
     </div>
